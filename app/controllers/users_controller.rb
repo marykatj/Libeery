ecb15377d43ead4fd28edb791ed0611f681ea010
Mary-Kate <marykatj@gmail.com>
2018-07-09 15:47:00 -0400

    Adding Post New Form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_action :fetch_user, only: [:show, :edit, :destroy]
+  before_action :fetch_user, only: [:show, :edit, :update, :destroy]
 
   def new
     @user = User.new
