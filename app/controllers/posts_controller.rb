29aeeec29cda85b92d752920d835001fd579d8dc
DevinPierce <devinmpierce@gmail.com>
2018-07-09 15:09:32 -0400

    started posts controller, added home route/view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,47 @@
 class PostsController < ApplicationController
+
+  before_action :fetch_post, only: [:show, :destroy, :edit, :update]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params(:description, :situation))
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @post.update(post_params(:description, :situation))
+      redirect_to @post #change to account creation path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = @post.user
+    @post.destroy
+    redirect_to @user
+  end
+
+  private
+  def post_params(*args)
+    params.require(:post).permit(args)
+  end
+
+  def fetch_post
+    @post = Post.find(params[:id])
+  end
 end
